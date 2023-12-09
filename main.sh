bad46c330c210320de69abc3ee8aa5882bee7d6c
Ubuntu <comp141@ML-RefVm-949980.zv0ixxhdtw1epaxq4zps5kvxsc.ex.internal.cloudapp.net>
2023-12-09 04:38:44 +0000

    First comment

--- main.sh
+++ main.sh
@@ -1,2 +1,11 @@
 extra credit assignment
-:wq
+this is for comp 141
+first, creat a new git repository on GitHub account
+name it comp141-extra-credit
+create a new local directory for this assignment
+name it extra-credit
+cd into it
+create main shell script
+main.sh
+run "git init" to initialize
+
